63c1651374d37ea9f307a9334de9d7c0b30a4f41
Atum Scott <atumscot@gmail.com>
2023-02-16 17:46:27 +0000

    Start on https://github.com/cyclestreets/cyclestreets-r/issues/46

--- R/journey2.R
+++ R/journey2.R
@@ -252,4 +252,35 @@ make_handle <- function(x){
   return(handle)
 }
 
+get_values = function(v, fun) {
+  sapply(v, function(x) fun(as.numeric(x)))
+}
+
+extract_values = function(x) stringr::str_split(x, pattern = ",")
+get_mean = function(v) get_values(v, fun = mean)
+get_sum = function(v) get_values(v, fun = sum)
+get_min = function(v) get_values(v, fun = min)
+get_max = function(v) get_values(v, fun = max)
+
+add_columns = function(r) {
+
+
+  elevations_list = extract_values(r_3$elevations)
+
+  r_3$elevations
+  r_3$elevation_min = get_min(elevations_list)
+  r_3$elevation_max = get_max(elevations_list)
+  distances_list = extract_values(r_3$distances)
+  r_3$segment_length = get_sum(distances_list)
+  r_3$elevation_change = r_3$elevation_max - r_3$elevation_min
+  r_3$gradient_segment = r_3$elevation_change / r_3$segment_length
+  r_3$gradient_smooth = cyclestreets::smooth_with_cutoffs(
+    r_3$gradient_segment,
+    r_3$elevation_change,
+    r_3$segment_length,
+    distance_cutoff = 50,
+    gradient_cutoff = 0.1,
+    n = 3,
+  )
+}
 
