532b2a9b1ae3a95a7a2c6c3a815e5627c8a5be44
robinlovelace <rob00x@gmail.com>
2023-02-17 13:44:47 +0000

    Comment out tests

--- R/batch.R
+++ R/batch.R
@@ -238,19 +238,19 @@ batch_jobdata = function(
   }
 }
 
-# # Tests:
-devtools::load_all()
-library(tidyverse)
-u = "https://github.com/cyclestreets/cyclestreets-r/releases/download/v0.5.3/cambridge-data.csv.gz"
-file = basename(u)
-download.file(u, file)
-res = batch_read(file)
-l_desire |>
-  slice(1:3) |>
-  mapview::mapview()
-res |>
-  filter(id %in% 1:3) |>
-  mapview::mapview()
+# # # Tests:
+# devtools::load_all()
+# library(tidyverse)
+# u = "https://github.com/cyclestreets/cyclestreets-r/releases/download/v0.5.3/cambridge-data.csv.gz"
+# file = basename(u)
+# download.file(u, file)
+# res = batch_read(file)
+# l_desire |>
+#   slice(1:3) |>
+#   mapview::mapview()
+# res |>
+#   filter(id %in% 1:3) |>
+#   mapview::mapview()
 
 
 batch_read = function(file) {
