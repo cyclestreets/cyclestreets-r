c3dc708db1e90ab8af24dd66a52539b6db82fa71
Robin Lovelace <rob00x@gmail.com>
2023-03-24 14:00:03 +0000

    Add more tests for #55

--- data-raw/test-batch-large-upload.R
+++ data-raw/test-batch-large-upload.R
@@ -5,8 +5,8 @@ library(cyclestreets)
 od_raw = pct::get_od()
 lsoas = pct::get_pct(layer = "z", national = TRUE, geography = "msoa")
 od = od_raw %>%
-  slice(seq(20000))
-summary(od$geo_code1 %in% lsoas)
+  slice(seq(110000))
+# summary(od$geo_code1 %in% lsoas)
 od = od::od_to_sf(x = od, z = lsoas)
 od$id = seq(nrow(od))
 nrow(od) # 19k
@@ -16,11 +16,25 @@ od_10k = od %>%
   slice(seq(10000))
 od_15k = od %>%
   slice(seq(15000))
+od_30k = od %>%
+  slice(seq(30000))
+od_35k = od %>%
+  slice(seq(35000))
+od_40k = od %>%
+  slice(seq(40000))
+od_50k = od %>%
+  slice(seq(50000))
+od_100k = od %>%
+  slice(seq(100000))
 
 sf::write_sf(od_10k, "od_10k.geojson")
 sf::write_sf(od_15k, "od_15k.geojson")
 
 batch(desire_lines = od_100, wait = FALSE, silent = FALSE, username = "robinlovelace")
 batch(desire_lines = od_10k, wait = FALSE, silent = FALSE, username = "robinlovelace")
-batch(desire_lines = od_15k, wait = FALSE, silent = FALSE, username = "robinlovelace")
+batch(desire_lines = od_30k, wait = FALSE, silent = FALSE, username = "robinlovelace")
+batch(desire_lines = od_35k, wait = FALSE, silent = FALSE, username = "robinlovelace")
+batch(desire_lines = od_40k, wait = FALSE, silent = FALSE, username = "robinlovelace")
+batch(desire_lines = od_50k, wait = FALSE, silent = FALSE, username = "robinlovelace")
+batch(desire_lines = od_100k, wait = FALSE, silent = FALSE, username = "robinlovelace")
 
